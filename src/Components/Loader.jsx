0d1a40333a3c559c0802f45ef17b580a0d6b7765
Alexander Lansdorp <alansdor@emeal.nttdata.com>
2022-11-02 17:43:22 -0500

    ISLA-39 layout loader

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -4,14 +4,19 @@ export default function Loader() {
         <>
             <div className="loader-background">
                 <div className="loader-container">
-                    <div className="loader-arm loader-arm-vertical arm-1"></div>
-                    <div className="loader-arm loader-arm-diagonal arm-2"></div>
-                    <div className="loader-arm loader-arm-horizontal arm-3"></div>
-                    <div className="loader-arm loader-arm-diagonal-2 arm-4"></div>
-                    <div className="loader-arm loader-arm-vertical arm-5"></div>
-                    <div className="loader-arm loader-arm-diagonal arm-6"></div>
-                    <div className="loader-arm loader-arm-horizontal arm-7"></div>
-                    <div className="loader-arm loader-arm-diagonal-2 arm-8"></div>
+                    <div class="lds-spinner"><div>
+                    </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div><div>
+                        </div></div>
                 </div>
             </div>
         </>
